981d74019b2e2d9674a8fe96a8d5ccdea6035488
Eric Wang <ericwang919@gmail.com>
2018-08-09 16:20:01 -0500

    undo num_workers

--- neuro_data/static_images/configs.py
+++ neuro_data/static_images/configs.py
@@ -86,8 +86,7 @@ class StimulusTypeMixin:
                 sampler = Sampler(ix, dataset.types, mode='longest')
             else:
                 sampler = Sampler(ix)
-            loaders[k] = DataLoader(dataset, sampler=sampler, 
-                                    batch_size=batch_size, num_workers=2)
+            loaders[k] = DataLoader(dataset, sampler=sampler, batch_size=batch_size)
             self.log_loader(loaders[k])
         return loaders
 
