ac3cf0bd949ff1d955caa0c64f0bf792b4b9347f
Eric Wang <ericwang919@gmail.com>
2018-10-25 20:58:31 -0500

    SubSubsetRandomSequentialSampler

--- neuro_data/utils/sampler.py
+++ neuro_data/utils/sampler.py
@@ -1,11 +1,12 @@
 from collections import Counter
 
 import torch
+import numpy as np
 from torch.utils.data.sampler import Sampler
 
 
 class SubsetSequentialSampler(Sampler):
-    """Samples elements randomly from a given list of indices, without replacement.
+    """Samples elements sequentially from a given list of indices, without replacement.
 
     Arguments:
         indices (list): a list of indices
@@ -19,6 +20,28 @@ class SubsetSequentialSampler(Sampler):
 
     def __len__(self):
         return len(self.indices)
+
+
+class SubSubsetRandomSequentialSampler(Sampler):
+    """Samples a subset from a dataset randomly. 
+    Then iterates through those indices sequentially.
+
+    Arguments:
+        indices (list): a list of indices
+        subsubset_size (int): size of subset of subset
+    """
+
+    def __init__(self, indices, subsubset_size):
+        self.indices = indices
+        self.subsubset_size = subsubset_size
+
+    def __iter__(self):
+        subsubset_indices = np.random.choice(self.indices, size=self.subsubset_size,
+                                             replace=False)
+        return (subsubset_indices[i] for i in range(len(subsubset_indices)))
+
+    def __len__(self):
+        return len(self.subsubset_size)
 
 
 class BalancedSubsetSampler(Sampler):
@@ -47,7 +70,8 @@ class BalancedSubsetSampler(Sampler):
         self.configure_sampler(indices, types, mode)
 
     def __iter__(self):
-        selection = torch.multinomial(self.weights, self.num_samples, self.replacement)
+        selection = torch.multinomial(
+            self.weights, self.num_samples, self.replacement)
         # print(Counter(self.types[self.indices[i]] for i in selection), len(selection)))
         return (self.indices[i] for i in selection)
 
