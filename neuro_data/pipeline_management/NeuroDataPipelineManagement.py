5904df0146a1baddb3334c070db18f258a4e21bb
synicix <synicix@gmail.com>
2019-12-09 17:27:21 -0600

    Change a few methods name, made them static, and add handling for mutiple scans in one function call

--- neuro_data/pipeline_management/NeuroDataPipelineManagement.py
+++ neuro_data/pipeline_management/NeuroDataPipelineManagement.py
@@ -13,40 +13,42 @@ class NeuroDataPipelineManagement():
     def __init__(self):
         pass
 
-    def manually_insert_area_for_scan(self, target_scan, area):
+    @staticmethod
+    def manually_insert_area_for_scan(target_scan, area):
         """
         Give a target_scan, it will label all neurons in there with the given area
 
-        Parameters:
-        target_scan : dict(animal_id, session, scan_idx)
-        area : str
+        Args:
+            target_scan (dict(animal_id, session, scan_idx)): A dict that contains the keys for a specific scan
+            area (str): area name to override label all the neurons with
 
         Returns:
-        None
+            None
         """
         neuron_unit_keys = (pipeline_fuse.ScanSet().Unit() & target_scan).fetch('KEY')
         for neuron_unit_key in neuron_unit_keys:
             neuron_unit_key['brain_area'] = area
             pipeline_anatomy.AreaMembership().insert1(neuron_unit_key, allow_direct_insert=True)
 
-    def manually_insert_layer_for_scan(self, target_scan, layer):
+    @staticmethod
+    def manually_insert_layer_for_scan(target_scan, layer):
         """
         Give a target_scan, it will label all neurons in there with the given layer
 
-        Parameters:
-        target_scan : dict(animal_id, session, scan_idx)
-        layer : str
+        Args:
+            target_scan (dict(animal_id, session, scan_idx)): A dict that contains the keys for a specific scan
+            layer (str): layer name to override label all the neurons with
 
         Returns:
-        None
+            None
         """
         neuron_unit_keys = (pipeline_fuse.ScanSet().Unit() & target_scan).fetch('KEY')
         for neuron_unit_key in neuron_unit_keys:
             neuron_unit_key['layer'] = layer
             pipeline_anatomy.LayerMembership().insert1(neuron_unit_key, allow_direct_insert=True)
 
-    # populate functions
-    def process_static_scan(self, target_scan):
+    @staticmethod
+    def process_static_scans(target_scans):
         """
         Function that goes and check for every table that needs to be populate as well as provide an option
         to manaully populate AreaMembership and LayerMembership, assuming that all the neurons can be label the same Area and Layer
@@ -54,136 +56,148 @@ class NeuroDataPipelineManagement():
 
         Please refer to neuro_data/notebooks/pipeline_management notebook for an example
 
-        Parameters:
-        target_scan : dict(animal_id, session, scan_idx)
+        Args:
+            target_scans (list(dict(animal_id, session, scan_idx))): A list of dicts where each dicts contains the keys for a specific scan
 
         Returns:
-        None
+            None
         """
-        print('Running preprocessing checks for ', target_scan)
 
-        # Check if the scan has been processed completely
-        if pipeline_fuse.ScanDone() & target_scan:
-            print('[Preprocessing Check]: ScanDone Check Passed')
-        else:
-            print('[Preprocessing Check]: ' + str(target_scan) + ' Scan has not been processed yet, please look into pipeline for details')
-            return
+        for target_scan in target_scans:
+            print('[NeuroDataPipelineManagement]: Processing ' + str(target_scan))
 
-        # Check if neurons area are labeled
-        if pipeline_anatomy.AreaMembership() & target_scan:
-            print('[Preprocessing Check]: AreaMembership Check Passed')
-        else:
-            print('[Preprocessing Check]: ' + str(target_scan) + " AreaMembership is not populated")
-            user_input = None
-            while user_input not in ['y', 'n']:
-                user_input = input('Should we manually insert if the area are known and all the same? [y/n]')
-                
-                if user_input == 'y':
-                    area = input('Input area to label neurons with [type exit for cancel]:')
-                    while area not in ['V1', 'LM', 'AL', 'RL', 'all-unknown']:
-                        if area == 'exit':
-                            return
-                        print('Invalid Area!')
-                        area = input('Input area to label neurons with:')
-                        
-                    self.manually_insert_area_for_scan(target_scan, area)
-                elif user_input == 'n':
-                    return
+            # Check if the scan has been processed completely
+            if pipeline_fuse.ScanDone() & target_scan:
+                print('[Preprocessing Check]: ScanDone Check Passed')
+            else:
+                print('[Preprocessing Check]: ' + str(target_scan) + ' Scan has not been processed yet, please look into pipeline for details')
+                return
 
-        # Check if neuron layers are labeled
-        if pipeline_anatomy.LayerMembership() & target_scan:
-            print('[Preprocessing Check]: LayerMembership Check Passed')
-        else:
-            print('[Preprocessing Check]: ' + str(target_scan) + " LayerMembership is not populated")
-
-            user_input = None
-            while user_input not in ['y', 'n']:
-                user_input = input('Should we manually insert if the layer are known and all the same? [y/n]')
-                
-                if user_input == 'y':
-                    layer = input('Input layer to label neurons with [type exit to cancel]:')
-                    while layer not in ['L1', 'L2/3', 'L4']:
-                        if layer == 'exit':
-                            return
-                        print('Invalid Layer!')
-                        layer = input('Input layer to label neurons with:')
-                        
+            # Check if neurons area are labeled
+            if pipeline_anatomy.AreaMembership() & target_scan:
+                print('[Preprocessing Check]: AreaMembership Check Passed')
+            else:
+                print('[Preprocessing Check]: ' + str(target_scan) + " AreaMembership is not populated")
+                user_input = None
+                while user_input not in ['y', 'n']:
+                    user_input = input('Should we manually insert if the area are known and all the same? [y/n]')
                     
-                    self.manually_insert_layer_for_scan(target_scan, layer)
-                elif user_input == 'n':
-                    return
+                    if user_input == 'y':
+                        area = input('Input area to label neurons with [type exit for cancel]:')
+                        while area not in ['V1', 'LM', 'AL', 'RL', 'all-unknown']:
+                            if area == 'exit':
+                                return
+                            print('Invalid Area!')
+                            area = input('Input area to label neurons with:')
+                            
+                        self.manually_insert_area_for_scan(target_scan, area)
+                    elif user_input == 'n':
+                        return
 
-        # Check pipeline_stimulus.Sync() table
-        if pipeline_stimulus.Sync() & target_scan:
-            print('[Preprocessing Check]: Sync Check Passed')
-        else:
-            print('[Preprocessing Check]: ' + str(target_scan) + ' pipeline_stimulus.Sync() table is not processed or failed to processed')
-            return
+            # Check if neuron layers are labeled
+            if pipeline_anatomy.LayerMembership() & target_scan:
+                print('[Preprocessing Check]: LayerMembership Check Passed')
+            else:
+                print('[Preprocessing Check]: ' + str(target_scan) + " LayerMembership is not populated")
 
-        # All tables requirements are met, begin neurodata dataset population
-        print('[Preprocessing Check]: All table requirements passed, beginning neuro_data populating:')
-        
-        # Get the ScanDone primary key reference
-        target_scan_done_key = (pipeline_fuse.ScanDone() & target_scan).fetch1('KEY')
+                user_input = None
+                while user_input not in ['y', 'n']:
+                    user_input = input('Should we manually insert if the layer are known and all the same? [y/n]')
+                    
+                    if user_input == 'y':
+                        layer = input('Input layer to label neurons with [type exit to cancel]:')
+                        while layer not in ['L1', 'L2/3', 'L4']:
+                            if layer == 'exit':
+                                return
+                            print('Invalid Layer!')
+                            layer = input('Input layer to label neurons with:')
+                            
+                        
+                        self.manually_insert_layer_for_scan(target_scan, layer)
+                    elif user_input == 'n':
+                        return
 
-        # Insert into StaticScanCandidate
-        if StaticScanCandidate & target_scan_done_key:
-            print('[NeuroData.Static Populate]: Scan has already been added to StaticScanCandidate')
-        else:
-            StaticScanCandidate.insert1(target_scan_done_key)
-            print('[NeuroData.Static Populate]: Successfully inserted Scan into StaticScanCandidate')
+            # Check pipeline_stimulus.Sync() table
+            if pipeline_stimulus.Sync() & target_scan:
+                print('[Preprocessing Check]: Sync Check Passed')
+            else:
+                print('[Preprocessing Check]: ' + str(target_scan) + ' pipeline_stimulus.Sync() table is not processed or failed to processed')
+                return
 
-        # Populating StaticScans
-        print("[NeuroData.Static Populate]: Populating StaticScan:")
-        StaticScan().populate(target_scan_done_key)
-
-        # Populate ConditionTier
-        print("[NeuroData.Static Populate]: Populating ConditionTier:")
-        ConditionTier.populate(target_scan_done_key)
-
-        # Check for incorrect flip times
-        print("[NeuroData.Static Populate]: Checking for Incorrect Flip Times:")
-        trials = (pipeline_stimulus.Trial() & target_scan).proj('flip_times').fetch(as_dict=True)
-        for trial in trials:
-            if trial['flip_times'].shape[1] != 3: # correct number of flips, hardcoded
-                ExcludedTrial.insert1(trial, ignore_extra_fields=True)
-
-        # Populate Frame
-        print("[NeuroData.Static Populate]: Populating Frame:")
-        Frame.populate(dict(preproc_id = 0))
-
-        # Populate InputResponse
-        print("[NeuroData.Static Populate]: Populating InputResponse:")
-        InputResponse().populate(target_scan_done_key, dict(preproc_id = 0))
-
-        # Populate Eye
-        print("[NeuroData.Static Populate]: Populating Eye:")
-        Eye().populate(target_scan_done_key)
-
-        # Populate Treadmill
-        print("[NeuroData.Static Populate]: Populating Treadmill:")
-        Treadmill().populate(target_scan_done_key)
-
-        # Insert Scan into StaticMultiDatasetGroupAssignment with whatever is the next highest_group_id
-        print("[NeuroData.Static Populate]: Inserting Scan into StaticMultiDatasetGroupAssignment with next largest group_id:")
-        target_input_response_key = (InputResponse & target_scan & dict(preproc_id=0)).fetch1('KEY')
-        if StaticMultiDatasetGroupAssignment & target_input_response_key:
-            print("[NeuroData.Static Populate]: Scan is already in StaticMultiDatasetGroupAssignment, skipping")
-        else:
-            target_input_response_key['group_id'] = StaticMultiDatasetGroupAssignment().fetch('group_id').max() + 1
-            target_input_response_key['description'] = 'Inserted from PipelineManagement'
-            StaticMultiDatasetGroupAssignment.insert1(target_input_response_key)
+            # All tables requirements are met, begin neurodata dataset population
+            print('[Preprocessing Check]: All table requirements passed, beginning neuro_data populating:')
             
-        # Fill StaticMultiDataset
-        print("[NeuroData.Static Populate]: Filling StaticMultiDataset:")
-        StaticMultiDataset().fill()
+            # Get the ScanDone primary key reference
+            target_scan_done_key = (pipeline_fuse.ScanDone() & target_scan).fetch1('KEY')
 
-        print('[NeuroData.Static Populate]: Generating HDF5 File')
-        InputResponse().get_filename(target_scan)
+            # Insert into StaticScanCandidate
+            if StaticScanCandidate & target_scan_done_key:
+                print('[NeuroData.Static Populate]: Scan has already been added to StaticScanCandidate')
+            else:
+                StaticScanCandidate.insert1(target_scan_done_key)
+                print('[NeuroData.Static Populate]: Successfully inserted Scan into StaticScanCandidate')
 
-        print('[PROCESSING COMPLETED FOR SCAN: ' + str(target_scan) + ']\n')
+            # Populating StaticScans
+            print("[NeuroData.Static Populate]: Populating StaticScan:")
+            StaticScan().populate(target_scan_done_key)
 
-    def delete_scans(self, target_scans):
+            # Populate ConditionTier
+            print("[NeuroData.Static Populate]: Populating ConditionTier:")
+            ConditionTier.populate(target_scan_done_key)
+
+            # Check for incorrect flip times
+            print("[NeuroData.Static Populate]: Checking for Incorrect Flip Times:")
+            trials = (pipeline_stimulus.Trial() & target_scan).proj('flip_times').fetch(as_dict=True)
+            for trial in trials:
+                if trial['flip_times'].shape[1] != 3: # correct number of flips, hardcoded
+                    ExcludedTrial.insert1(trial, ignore_extra_fields=True)
+
+            # Populate Frame
+            print("[NeuroData.Static Populate]: Populating Frame:")
+            Frame.populate(dict(preproc_id = 0))
+
+            # Populate InputResponse
+            print("[NeuroData.Static Populate]: Populating InputResponse:")
+            InputResponse().populate(target_scan_done_key, dict(preproc_id = 0))
+
+            # Populate Eye
+            print("[NeuroData.Static Populate]: Populating Eye:")
+            Eye().populate(target_scan_done_key)
+
+            # Populate Treadmill
+            print("[NeuroData.Static Populate]: Populating Treadmill:")
+            Treadmill().populate(target_scan_done_key)
+
+            # Insert Scan into StaticMultiDatasetGroupAssignment with whatever is the next highest_group_id
+            print("[NeuroData.Static Populate]: Inserting Scan into StaticMultiDatasetGroupAssignment with next largest group_id:")
+            target_input_response_key = (InputResponse & target_scan & dict(preproc_id=0)).fetch1('KEY')
+            if StaticMultiDatasetGroupAssignment & target_input_response_key:
+                print("[NeuroData.Static Populate]: Scan is already in StaticMultiDatasetGroupAssignment, skipping")
+            else:
+                target_input_response_key['group_id'] = StaticMultiDatasetGroupAssignment().fetch('group_id').max() + 1
+                target_input_response_key['description'] = 'Inserted from PipelineManagement'
+                StaticMultiDatasetGroupAssignment.insert1(target_input_response_key)
+                
+            # Fill StaticMultiDataset
+            print("[NeuroData.Static Populate]: Filling StaticMultiDataset:")
+            StaticMultiDataset().fill()
+
+            print('[NeuroData.Static Populate]: Generating HDF5 File')
+            InputResponse().get_filename(target_scan)
+
+            print('[PROCESSING COMPLETED FOR SCAN: ' + str(target_scan) + ']\n')
+
+    def delete_scans_from_pipeline(self, target_scans):
+        """
+        Reverese what process_static_scans for the given target_scans
+
+        Args:
+            target_scans (list(dict(animal_id, session, scan_idx))): A list of dicts where each dicts contains the keys for a specific scan
+
+        Returns:
+            None
+        """
+
         (InputResponse.Input() & target_scans).delete_quick()
         (Frame() & (ConditionTier & target_scans)).delete_quick()
         (ConditionTier() & target_scans).delete_quick()
