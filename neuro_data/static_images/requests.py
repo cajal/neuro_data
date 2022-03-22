1ccbf23431b579137ceca8b2d171c0db592e7b9b
Zhuokun Ding <zkding@outlook.com>
2022-03-22 17:29:36 -0500

    Refactor request table

--- neuro_data/static_images/requests.py
+++ neuro_data/static_images/requests.py
@@ -1,14 +1,13 @@
 from os import sched_getaffinity
 from .dataset_config import (
     InputConfig,
-    ResponseConfig,
     TierConfig,
     LayerConfig,
     AreaConfig,
     StatsConfig,
 )
 from .data_schemas import StaticScan
-from .ds_pipe import DynamicScan
+from .ds_pipe import DvScanInfo
 import datajoint as dj
 
 schema = dj.schema("neurodata_static")
@@ -17,10 +16,9 @@ schema = dj.schema("neurodata_static")
 @schema
 class DynamicStaticNoBehRequest(dj.Manual):
     definition = """
-    -> DynamicScan.proj(dynamic_session='session', dynamic_scan_idx='scan_idx')
+    -> DvScanInfo.proj(dynamic_session='session', dynamic_scan_idx='scan_idx')
     -> StaticScan.proj(static_session='session', static_scan_idx='scan_idx')
     -> InputConfig
-    -> ResponseConfig
     -> TierConfig
     -> LayerConfig
     -> AreaConfig
