4d30d4fdb3816b562973c17c524704390c4dbc73
Zhuokun Ding <zkding@outlook.com>
2022-03-22 17:29:21 -0500

    Add DvScanInfo for available dynamic models

--- neuro_data/static_images/ds_pipe.py
+++ neuro_data/static_images/ds_pipe.py
@@ -2,6 +2,7 @@ import datajoint as dj
 from neuro_data.utils.config import ConfigBase
 import pandas as pd
 import numpy as np
+from neuro_data.static_images.data_schemas import StaticScan
 
 experiment = dj.create_virtual_module("experiment", "pipeline_experiment")
 fuse = dj.create_virtual_module("fuse", "pipeline_fuse")
@@ -16,78 +17,12 @@ dv_nn6_train = dj.create_virtual_module("dv_nn6_train", "dv_nns_v6_train")
 dv_nn6_model = dj.create_virtual_module("dv_nn6_model", "dv_nns_v6_model")
 dv_nn6_scan = dj.create_virtual_module("dv_nn6_scan", "dv_nns_v6_scan")
 dv_nn6_pipe = dj.create_virtual_module("dv_nn6_pipe", "dv_nns_v6_ds_pipe")
+dv_scan1_scan_dataset = dj.create_virtual_module(
+    "dv_scan1_scan_dataset", "dv_scans_v1_scan_dataset"
+)
+dv_scan1_scan = dj.create_virtual_module("dv_scan1_scan", "dv_scans_v1_scan")
 
 schema = dj.schema("neurodata_static")
-
-
-@schema
-class DynamicScanCandidate(dj.Manual):
-    definition = """ # list of scans to process
-
-    -> fuse.ScanDone
-    ---
-    candidate_notes='' : varchar(1024)
-    """
-
-    @staticmethod
-    def fill(
-        key, candidate_notes="", segmentation_method=6, spike_method=6, pipe_version=1
-    ):
-        """Fill an entry with key"""
-        DynamicScanCandidate().insert1(
-            {
-                "segmentation_method": segmentation_method,
-                "spike_method": spike_method,
-                "pipe_version": pipe_version,
-                **key,
-                "candidate_notes": candidate_notes,
-            },
-            skip_duplicates=True,
-        )
-
-
-@schema
-class DynamicScan(dj.Computed):
-    definition = """ # gatekeeper for scan and preprocessing settings
-    
-    -> fuse.ScanDone
-    """
-
-    class Unit(dj.Part):
-        definition = """
-        # smaller primary key table for data
-        -> master        
-        unit_id              : int                          # unique per scan & segmentation method
-        ---
-        -> fuse.ScanSet.Unit
-        """
-
-    key_source = (
-        fuse.ScanDone()
-        & DynamicScanCandidate
-        & "spike_method=6 and segmentation_method=6"
-    )
-
-    @staticmethod
-    def complete_key(key):
-        return dict(
-            (
-                dj.U("segmentation_method", "pipe_version")
-                & (meso.ScanSet.Unit() & key)
-            ).fetch1(dj.key),
-            **key
-        )
-
-    def make(self, key):
-        self.insert(fuse.ScanDone() & key, ignore_extra_fields=True)
-        pipe = (fuse.ScanDone() & key).fetch1("pipe")
-        pipe = dj.create_virtual_module(pipe, "pipeline_" + pipe)
-        self.Unit().insert(
-            fuse.ScanDone * pipe.ScanSet.Unit
-            & key
-            & dict(pipe_version=1, segmentation_method=6, spike_method=5),
-            ignore_extra_fields=True,
-        )
 
 
 def dv_nn6_models():
@@ -126,9 +61,16 @@ class DvModelConfig(ConfigBase, dj.Lookup):
             dynamic_scan, n_units = (dv_nn6_scan.Scan & dynamic_scan & self).fetch1(
                 dj.key, "n_units"
             )
-            units = (self * dv_nn6_scan.Scan.Unit & dynamic_scan).proj()
+            units = (
+                self
+                * dv_nn6_scan.Scan.Unit
+                * dv_nn6_scan.ScanConfig().Scan1()
+                * dv_scan1_scan_dataset.Preprocess
+                * dv_scan1_scan.ResponseId
+                & dynamic_scan
+            )
             unit_keys = units.fetch(
-                *fuse.ScanSet.Unit.primary_key,
+                *(fuse.ScanDone * fuse.ScanSet.Unit).primary_key,
                 as_dict=True,
                 order_by="nn_response_index"
             )
@@ -161,3 +103,57 @@ class DvModelConfig(ConfigBase, dj.Lookup):
             )
             assert len(cond_df) == len(resp_df)
             return np.stack(resp_df.response.values)  # (n_images, n_units)
+
+
+@schema
+class DvScanInfo(dj.Computed):
+    definition = """
+    -> StaticScan
+    -> DvModelConfig
+    ---
+    n_units         : int unsigned      # number of units
+    """
+
+    class Unit(dj.Part):
+        definition = """
+        -> master
+        -> StaticScan.Unit
+        ---
+        response_index      : int unsigned      # index of unit in response vector
+        """
+
+    @property
+    def key_source(self):
+        keys = fuse.ScanDone * DvModelConfig
+        key = [
+            dv_nn6_models() * DvModelConfig.Nn6 * dv_nn6_scan.Scan,
+        ]
+        return keys & key
+
+    def make(self, key):
+        unit_keys = DvModelConfig().part_table(key).unit_keys(key)
+        unit_keys = [
+            dict(unit_key, dv_model_hash=key["dv_model_hash"]) for unit_key in unit_keys
+        ]
+        self.insert1(dict(key, n_units=len(unit_keys)))
+
+        self.Unit.insert(
+            [dict(unit_key, response_index=i) for i, unit_key in enumerate(unit_keys)]
+        )
+
+    def responses(self, dynamic_scan, trial_idx, condition_hashes, key=None):
+        if key is None:
+            key, n_units = self.fetch1(dj.key, "n_units")
+        else:
+            key, n_units = (self & key).fetch1(dj.key, "n_units")
+
+        dv_conf = DvModelConfig().part_table(key)
+        responses = dv_conf.responses(dynamic_scan, trial_idx, condition_hashes)
+        assert responses.shape[1] == n_units
+        return responses
+
+    def unit_keys(self, dynamic_scan, key=None):
+        key = self.fetch1(dj.key) if key is None else key
+        dv_conf = DvModelConfig().part_table(key)
+        unit_keys = dv_conf.unit_keys(dynamic_scan)
+        return unit_keys
