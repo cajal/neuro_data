use consistent norm_tier for normalizing inputs, responses, behavior, and fix minor bugs

neuro_data/static_images/data_schemas.py
@@ -346,7 +346,7 @@ class Preprocessing(dj.Lookup):
     filter           : varchar(24)  # filter type for window extraction
     gamma            : boolean      # whether to convert images to luminance values rather than pixel intensities
     linear_mon       : boolean      # whether the monitor has been linearized (i.e. pixel and luminance space form a linear relationship)
-    input_stats      : varchar(16)  # input dataset used for computing statistics and normalization, options include 'train', 'validation', 'test', and 'all'
+    norm_tier        : varchar(16)  # the tier(s) used for computing statistics and normalizing data, options include 'train', 'validation', 'test', and 'all'
     stats_per_input  : boolean      # whether to compute stats of each individual input and then average
     data_source      : varchar(16)  # source of the input-response dataset, options include 'brain' or certain type of digital twin 
     gt_availability  : boolean      # whether groundtruth in vivo response is available
@@ -356,7 +356,7 @@ class Preprocessing(dj.Lookup):
                 (2, 0.05, 0.5, 72, 128, 'hamming', 0, 'train', 0, 0, 'brain', 1),
                 (3, 0.05, 0.5, 36, 64, 'hamming', 1, 'train', 0, 0, 'brain', 1),
                 (4, 0.03, 0.5, 36, 64, 'hamming', 0, 'train', 0, 0, 'brain', 1),
-                (5, 0.05, 0.5, 36, 64, 'hamming', 0, 'all', 1, 0, 'brain', 1),
+                (5, 0.05, 0.5, 36, 64, 'hamming', 0, 'all', 1, 0, 'brain', 1), # BUGGY: norm_tier = 'all' was used only for input images, norm_tier = 'train' for responses and behavioral data
                 (6, 0.05, 0.5, 36, 64, 'hamming', 0, 'train', 1, 1, 'brain', 1),
                 (7, 0.05, 0.5, 36, 64, 'hamming', 1, 'train', 1, 0, 'brain', 1),
                 (8, 0.05, 0.5, 36, 64, 'hamming', 0, 'train', 1, 0, 'dynamic model', 1),
@@ -365,6 +365,7 @@ class Preprocessing(dj.Lookup):
                 (11, 0.05, 0.2, 36, 64, 'hamming', 0, 'train', 1, 0, 'brain', 1),
                 (12, 0.05, 0.3, 36, 64, 'hamming', 0, 'train', 1, 0, 'brain', 1),
                 (13, 0.05, 0.4, 36, 64, 'hamming', 0, 'train', 1, 0, 'brain', 1),
+                (14, 0.05, 0.5, 36, 64, 'hamming', 0, 'test', 1, 0, 'brain', 1), # only useful for dynamic scans including static oracles (so the only existing tier is 'test')
                 ]
 
 
@@ -599,10 +600,9 @@ class InputResponse(dj.Computed, FilterMixin):
                            for i, trial_key in enumerate(compress(trial_keys, valid))])
 
     def compute_data(self, key):
-        preproc_params = (Preprocessing & key).fetch1()
-
         key = dict((self & key).fetch1(dj.key), **key)
         log.info('Computing dataset for\n' + pformat(key, indent=20))
+        preproc_params = (Preprocessing & key).fetch1()
 
         # meso or reso?
         pipe = (fuse.ScanDone() * StaticScan() & key).fetch1('pipe')
@@ -813,10 +813,9 @@ class InputResponse(dj.Computed, FilterMixin):
             return ret
 
         # --- compute statistics
-        log.info('Computing statistics on training dataset')
-        response_statistics = run_stats(lambda ix: responses[ix], types, tiers == 'train', axis=0)
-
-        ix = np.arange(len(tiers)) if preproc_params['input_stats'] == 'all' else tiers == preproc_params['input_stats']
+        log.info('Computing statistics on {} dataset(s)'.format(preproc_params['norm_tier']))
+        ix = np.arange(len(tiers)) if preproc_params['norm_tier'] == 'all' else tiers == preproc_params['norm_tier']
+        response_statistics = run_stats(lambda ix: responses[ix], types, ix, axis=0)
         input_statistics = run_stats_input(lambda ix: images[ix], types, ix, axis=0, per_input=preproc_params['stats_per_input']) 
 
         statistics = dict(
@@ -826,8 +825,8 @@ class InputResponse(dj.Computed, FilterMixin):
 
         if include_behavior:
             # ---- include statistics
-            behavior_statistics = run_stats(lambda ix: behavior[ix], types, tiers == 'train', axis=0)
-            eye_statistics = run_stats(lambda ix: pupil_center[ix], types, tiers == 'train', axis=0)
+            behavior_statistics = run_stats(lambda ix: behavior[ix], types, ix, axis=0)
+            eye_statistics = run_stats(lambda ix: pupil_center[ix], types, ix, axis=0)
 
             statistics['behavior'] = behavior_statistics
             statistics['pupil_center'] = eye_statistics